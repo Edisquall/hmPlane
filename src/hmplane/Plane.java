0aa482949832c5d058d05bf533d721d5a08a2627
Edisquall <152781383+Edisquall@users.noreply.github.com>
2023-12-03 13:41:40 +0000

    Message
    Creating a message in a new branch

--- src/hmplane/Plane.java
+++ src/hmplane/Plane.java
@@ -9,9 +9,13 @@ package hmplane;
  * @author Edi
  */
 public class Plane {
-    String company;
     String model;
+    String company;
     int seats;
-    int numWheels;
+    int destination;
+    
+    public void flight(direction){
+        System.out.printf("The flight %s with %s has %f going to %f is about to take off" ,model, company, seats, destination);
+    }
     
 }
